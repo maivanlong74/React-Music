cd671a0cfaf48a02499a4d860198c240c43068bf
maivanlong74 <maivanlong143@gmail.com>
2023-08-21 20:49:26 +0700

    Message

--- src/Components/Library/Animation-Img/AnimationImg.js
+++ src/Components/Library/Animation-Img/AnimationImg.js
@@ -6,12 +6,18 @@ import { AiOutlineDown } from "react-icons/ai";
 const AnimationImg = ({ handleScrollClick }) => {
   const { DataSongs, song } = useContext(Songs);
   const [activeImageIndex, setActiveImageIndex] = useState(0);
-  const images = DataSongs.map((dem, index) => dem.links.images[1].url);
+  
+  const images = DataSongs.map((dem, index) => ({
+    nen: dem.links.images[1].url,
+    nameSonger: dem.author,
+    nameMusic: dem.name,
+    avtSonger: dem.links.images[0].url
+  }));
 
   useEffect(() => {
     const interval = setInterval(() => {
       setActiveImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 3000);
+    }, 2000);
 
     return () => clearInterval(interval);
   }, [images.length]);
@@ -22,16 +28,23 @@ const AnimationImg = ({ handleScrollClick }) => {
         <div className="scroll-down">
           <AiOutlineDown className="icon-scroll" id="click-scroll" onClick={handleScrollClick} />
         </div>
+        <div className="backround-rgba"></div>
+        {images.map((path, index) => (
+          <div key={index} className={`Img-Item ${index === activeImageIndex ? 'active' : ''}`} style={{ display: index === activeImageIndex ? 'block' : 'none' }}>
+            <img src={path.nen} alt={`Image ${index}`} />
 
-        {images.map((imgSrc, index) => (
-          <img
-            key={index}
-            src={imgSrc}
-            className={`Img-Item ${index === activeImageIndex ? 'active' : ''}`}
-            style={{ display: index === activeImageIndex ? 'block' : 'none' }}
-            alt={`Image ${index}`}
-          />
+            <div className="Info-Music">
+              <div className="avt-singer">
+                <img src={path.avtSonger} alt={`Singer ${index}`} />
+              </div>
+              <div className="info-singer">
+                <h1>{path.nameMusic}</h1>
+                <p>{path.nameSonger}</p>
+              </div>
+            </div>
+          </div>
         ))}
+
       </div>
     </>
   );
--- src/Components/Library/ShowMusic/ShowMusic.js
+++ src/Components/Library/ShowMusic/ShowMusic.js
@@ -1,12 +1,82 @@
-import { memo } from "react";
+import { memo, useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom"
 import "./ShowMusic.scss";
+import { Songs } from "../../../Connect/Context";
 
 const ShowMusic = () => {
-    return(
+    const { DataSongs } = useContext(Songs);
+    const [isExpanded, setIsExpanded] = useState([]);
+    const listSinger = DataSongs.map((dem, index) => ({
+        nameSinger: dem.author,
+        avtSinger: dem.links.images[0].url
+    }));
+
+    const limitedImages = listSinger.slice(0, 9);
+
+    // Loại bỏ giá trị trùng nhau của dem.author
+    const uniqueAuthors = [];
+    const filteredImages = limitedImages.filter((limitedImages) => {
+        if (!uniqueAuthors.includes(limitedImages.nameSinger)) {
+            uniqueAuthors.push(limitedImages.nameSinger);
+            return true;
+        }
+        return false;
+    });
+
+    const toggleContent = () => {
+        setIsExpanded(!isExpanded);
+    };
+    useEffect(() => {
+        const ListMusicItems = document.querySelectorAll('.List-Music-item');
+        if (ListMusicItems.length > 0) {
+            ListMusicItems.forEach(item => {
+                const ShowMusic = item.querySelector('.Music-item-list');
+                if (ShowMusic) {
+                    if (isExpanded) {
+                        ShowMusic.style.height = "auto";
+                        item.classList.add("padding-bottom");
+                    } else {
+                        ShowMusic.style.height = "31vh";
+                        item.classList.remove("padding-bottom");
+                    }
+                }
+            });
+        }
+    }, [isExpanded]);
+
+    return (
         <div className="Music-item">
-        
+
+            <ul className="All-Music-Item">
+                <li className="List-Music-item">
+                    <div className="Music-item-content">
+                        <div className="content-a"><h1>Các ca sĩ hot</h1></div>
+                        <div className="content-b">
+                            <p className="content" onClick={toggleContent}>
+                                {isExpanded ? "Ẩn bớt" : "Xem thêm"}
+                            </p>
+                        </div>
+                    </div>
+                    <div className="Music-item-list">
+                        <ul>
+                            {filteredImages.map((song, index) => (
+                                <li key={index}>
+                                    <Link to="{}">
+                                        <img src={song.avtSinger}></img>
+                                        <div className="name-singer">
+                                            <p>{song.nameSinger}</p>
+                                        </div>
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+
         </div>
     )
 }
 
-export default memo(ShowMusic); 
+export default memo(ShowMusic);
+
